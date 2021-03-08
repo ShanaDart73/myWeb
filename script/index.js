ce25810042ca5c00a9801de3c0795566710881e9
Carole Kayungu <carole.kayungu@gmail.com>
2021-03-08 01:14:46 +0100

    modified script and style files

--- script/index.js
+++ script/index.js
@@ -1,10 +1,36 @@
 
-const isHidden = document.querySelector('#is-hidden');
-isHidden.hidden = true;
+const isNavHidden = document.querySelector('#isNavHidden');
+isNavHidden.hidden = true;
 
 function handler() {
-  (!isHidden.hidden) 
-  ? isHidden.hidden = true 
-  : isHidden.hidden = false;
+  (!isNavHidden.hidden) 
+  ? isNavHidden.hidden = true 
+  : isNavHidden.hidden = false;
 }
 
+const recentAdded = document.querySelector('.recent-added');
+recentAdded.hidden = true;
+
+function displayRecentAdded() {
+  (!recentAdded.hidden) 
+  ? recentAdded.hidden = true 
+  : recentAdded.hidden = false;
+}
+
+const ongoingSeries = document.querySelector('.ongoing-series');
+ongoingSeries.hidden = true;
+
+function displayOngoingSeries() {
+  (!ongoingSeries.hidden) 
+  ? ongoingSeries.hidden = true 
+  : ongoingSeries.hidden = false;
+}
+
+const genres = document.querySelector('.genres');
+genres.hidden = true;
+
+function displayGenres() {
+  (!genres.hidden) 
+  ? genres.hidden = true 
+  : genres.hidden = false;
+}
